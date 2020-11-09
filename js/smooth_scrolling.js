a75f00c9522cee28636a89b9f35155a935c39625
kostya <matvienkoigor956@gmail.com>
2020-11-09 13:13:43 +0200

    update breakpoints

--- js/smooth_scrolling.js
+++
@@ -1,6 +0,0 @@
-$(".class").on("click", function(event) {
-    event.preventDefault();
-    var id = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({ scrollTop: top }, 500);
-});
